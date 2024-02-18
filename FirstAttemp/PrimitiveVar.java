ed64ea6ab5a0bca3b3778c0f1d220f913ae07a69
AgnesVarnagy <agnes.vrngy@gmail.com>
2024-02-18 18:03:41 +0100

    added git inti and create repo

---
+++ FirstAttemp/PrimitiveVar.java
@@ -0,0 +1,17 @@
+package FirstAttemp;
+
+public class PrimitiveVar {
+    public static void main(String[] args) {
+    }
+
+    byte num1 = 11;
+    short num2 = 5000;
+    int num3 = 1000000;
+    long num4 = 1000000000L;
+    float num5 = 100.123456f;
+    double num6 = 5000.456789456;
+
+    System.out.println("byteVar "+ num1);
+
+}
+}
